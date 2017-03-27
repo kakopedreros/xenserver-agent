76c63ab74b759a7cbec6f61216c902073095d777
Kako Pedreros <kakopedreros@users.noreply.github.com>
2017-03-26 22:58:07 -0300

    Update and rename xenserver.sh to xenserver-host.sh

--- xenserver-host.sh
+++ xenserver-host.sh
@@ -41,22 +41,3 @@ for key in "${!array_cpu[@]}"
 do
     echo cpu$key: $(xe host-cpu-param-get uuid="${array_cpu[$key]}" param-name=utilisation)
 done
-
-# VMs on Host
-
-vm_host=$(xe vm-list resident-on=$host_uuid --minimal)
-
-IFS=,
-array_vm=($vm_host)
-
-for key in "${!array_vm[@]}"
-do
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=name-label)
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=VCPUs-number)
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=VCPUs-utilisation --minimal)
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=memory-actual)
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=power-state)
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=os-version)
-    echo vm$key: $(xe vm-param-get uuid="${array_vm[$key]}" param-name=networks)
-    echo ''
-done
